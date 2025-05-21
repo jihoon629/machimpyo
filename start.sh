3967d873c86c2540061e8417c608e98d27bc0530
jihoon629 <jihonn629@gmail.com>
2025-05-21 19:37:07 +0900

    2트

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
 cd ./basic-network/
 cp -r ../../fabric-samples/bin/ .
-echo "복사됨"
+echo "바이너리 복사됨"
+cd ../
+
+cd ./application # application 폴더로 이동
+npm install connect-history-api-fallback
+echo "아님멜루"
